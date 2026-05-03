50c77dc97e0c67c869398a29982e66786ab4e981
ROMEO <rgarges7@gmail.com>
2026-05-03 04:51:03 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'romeo-codepad-v2';
+const assets = [
+  '/',
+  '/index.html',
+  '/matches.html',
+  '/manifest.json',
+  'https://i.postimg.cc/Gp5QYTPd/Picsart-26-05-03-04-30-32-110.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
